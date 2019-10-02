430eca187ba70bbfe34adc39feb2d7ee82e99e24
AkashSh <akash.sharma@hotmail.de>
2019-10-02 02:42:51 +0200

    Add files via upload

---
+++ Assets/Directionguid.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Directionguid : MonoBehaviour
+{
+    public GameObject go1;
+    public GameObject go2;
+    public GameObject go3;
+    public GameObject go4;
+    [SerializeField]
+    int a;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("Invoke", 1, 1 / 2f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(CoroutineTest());
+
+        }
+
+
+        IEnumerator CoroutineTest()
+        {
+            go1.SetActive(false);
+            go2.SetActive(false);
+            go3.SetActive(false);
+            go4.SetActive(false);
+
+    
+
+
+            yield return new WaitForSeconds(3);
+            /*int[] array;
+            array = new int[4];
+
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;*/
+
+
+
+            //if (a == 1)
+            //{
+                go1.SetActive(true);
+                go2.SetActive(false);
+                go3.SetActive(false);
+                go4.SetActive(false);
+
+                yield return new WaitForSeconds(3);
+
+                go1.SetActive(false);
+                go2.SetActive(false);
+                go3.SetActive(false);
+                go4.SetActive(false);
+
+           // }
+
+           /* if (a == 2)
+            {
+                go1.SetActive(false);
+                go2.SetActive(true);
+                go3.SetActive(false);
+                go4.SetActive(false);
+            }
+
+
+            if (a == 3)
+            {
+                go1.SetActive(false);
+                go2.SetActive(false);
+                go3.SetActive(true);
+                go4.SetActive(false);
+            }
+
+            if (a == 4)
+            {
+                go1.SetActive(false);
+                go2.SetActive(false);
+                go3.SetActive(false);
+                go4.SetActive(true);
+            }*/
+        }
+
+    }
+    /*void Invoke()
+    {
+        a = Random.Range(1, 5);
+
+    }*/
+}      /*if (a == 1)
+            {
+                go1.SetActive(true);
+                go2.SetActive(false);
+                go3.SetActive(false);
+                go4.SetActive(false);
+
+            }
+            if (a == 2)
+            {
+                go1.SetActive(false);
+                go2.SetActive(true);
+                go3.SetActive(false);
+                go4.SetActive(false);
+            }
+
+            if (a == 3)
+            {
+                go1.SetActive(false);
+                go2.SetActive(false);
+                go3.SetActive(true);
+                go4.SetActive(false);
+            }
+
+            if (a == 4)
+            {
+                go1.SetActive(false);
+                go2.SetActive(false);
+                go3.SetActive(false);
+                go4.SetActive(true);
+            }
+
+        }
+                InvokeRepeating("Invoke", 1, 1 / 2f);
+
+            void Invoke()
+            {
+                int a = Random.Range(1, 4);
+            }
+
+
+    } */
+
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject stimulater;
+    public float frequency; 
+    // Update is called once per frame
+ private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            InvokeRepeating("invoke", 1, 1 / frequency);
+        }
+    }
+    void invoke ()
+    {
+        //Debug.Log(panel.activSelf);
+        stimulater.SetActive(!stimulater.activeSelf);
+    }
+}
+
+/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickering : MonoBehaviour {
+    public GameObject control;
+    public GameObject panel;
+    public float frequency;
+
+    // Use this for initialization
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            InvokeRepeating("invoke", 1, 1 / frequency);
+        }
+
+        
+
+        
+    }
+    // Update is called once per frame
+
+
+    void invoke()
+    {
+       // Debug.Log(panel.activeSelf);
+
+        panel.SetActive(!panel.activeSelf);
+        control.SetActive(!control.activeSelf);
+    }
+
+
+}
+*/
